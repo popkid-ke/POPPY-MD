07b013972256739d7735b185126ac3e6c58ba747
popkid-ke <kidreen254@gmail.com>
2024-07-26 11:45:17 +0300

    Update canvacord.js

--- commandes/canvacord.js
+++ commandes/canvacord.js
@@ -1,4 +1,4 @@
-const { skipper } = require("../framework/skipper");
+const { popkid } = require("../framework/popkid");
 const canvacord = require("canvacord");
 const {uploadImageToImgur} = require("../framework/imgur")
 
